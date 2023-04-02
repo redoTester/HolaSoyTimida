bbbc444a1553c3f2000b5c01a0ec49505ad98925
redoTester <rensopalomyno@gmail.com>
2023-04-02 17:04:53 -0500

    Nara te extraño

---
+++ AppWeb01T_Core/Controllers/CarrerasController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppWeb01T_Core.Controllers
+{
+    public class CarrerasController : Controller
+    {
+        public IActionResult Index()
+        {
+            //enviar la fecha a traves del viewdata de clave fecha
+            ViewData["fecha"] = DateTime.Now.ToString();
+            return View();
+        }
+    }
+}
---
+++ AppWeb01T_Core/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppWeb01T_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
